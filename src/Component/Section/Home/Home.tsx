4c9b1131595d8841dbfeb1a1c8f7f13095bc8160
Lemine-Mahjoub <leminemahjoub@gmail.com>
2024-10-09 01:00:52 +0100

    :sparkles: Initialisation Composant Principaux :sparkles:

---
+++ src/Component/Section/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Section</h1>
+      <p>This is the home section of the portfolio.</p>
+    </div>
+  );
+};
